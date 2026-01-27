4bab93f55f539111d3e2ae5f0aa594d8e60a0fe7
nobertidu <118987755+nobertidu@users.noreply.github.com>
2026-01-27 17:58:51 +0100

    google analytics added

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Toaster } from 'react-hot-toast'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Toaster } from "react-hot-toast";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// Google Analytics
+const script1 = document.createElement("script");
+script1.async = true;
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-YDL9J9VVRG";
+document.head.appendChild(script1);
+
+const script2 = document.createElement("script");
+script2.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-YDL9J9VVRG');
+`;
+document.head.appendChild(script2);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   // <React.StrictMode>
-    <Router >
-      <Toaster position="top-right" reverseOrder={false} />
-      <App />
-    </Router>
+  <Router>
+    <Toaster position="top-right" reverseOrder={false} />
+    <App />
+  </Router>,
   // {/* </React.StrictMode> */}
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
